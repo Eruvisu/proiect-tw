fe76da90035af827a3da4d9ec790288dfe58d672
Danut Hostiuc <danut.hostiuc@gmail.com>
2017-06-05 02:52:51 +0300

    Colectii

--- Proiect/colectii.php
+++ Proiect/colectii.php
@@ -49,7 +49,6 @@
         </div>
 
         <article>
-		
 		<?php
 			require_once("php/db_const.php");
 			$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -60,8 +59,20 @@
 			}
 			$sql = "SELECT CONCAT(image_path, artefact_name, extension) AS image FROM artefacts";
 			$result = $mysqli->query($sql);
+			//$iterator = 0;
 			while($row = mysqli_fetch_row($result)){
-				echo '<img src="'.$row[0].'">';
+				/*if($iterator == 0){
+					echo '<tr><td><img src="'.$row[0].'"></td>';
+				}
+				if($iterator == 1){
+					echo '<td><img src="'.$row[0].'"></td>';
+				}
+				if($iterator == 2){
+					echo '<td><img src="'.$row[0].'"></td></tr>';
+					$iterator = -1;
+				}
+				$iterator++;*/
+				echo '<div class="responsive"><div class="gallery"><a target="_blank" href="'.$row[0].'"><img src="'.$row[0].'" width="300" height="200"></a><div class="desc">Description</div></div></div>';
 			}
 			$result->close();
 		?>
