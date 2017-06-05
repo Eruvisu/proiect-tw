c4effde1f8ac9ea09a81dfc055208bdcc66acecb
Eruvisu <Eruvisu@users.noreply.github.com>
2017-06-06 02:09:48 +0300

    Added a commentary

--- Proiect/How-To-Do-A-New-Page.php
+++ Proiect/How-To-Do-A-New-Page.php
@@ -1,6 +1,7 @@
 <?php
 $the_title='New Page Title';
 $the_content='This is the content of the page'; //put here the name of the file with the content ie: $the_content='colectii-content.php';
+//For an example look at colectii.php and colectii-content.php
 ?>
 
 <?php include('./php/template/site-template.php'); ?>
